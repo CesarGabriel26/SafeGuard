fed425583550290d68a8a6cef46b55caae2e7db2
CesarGabriel26 <cesargabrielsousasousa196@gmail.com>
2024-04-30 12:44:01 -0300

    atualização 30/05/2024 - 12:43

--- API/app.py
+++ API/app.py
@@ -2,14 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 
 from rotas.colaboradores import colaboradores_bp
-# from rotas.api_api_colaborador_EPIs import colaborador_EPIs_bp
+from rotas.epi import Epis_bp
 # from rotas.rota_api_colaboradores_EPIs_bp import colaboradores_EPIs_bp
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(colaboradores_bp, url_prefix='/colaboradores')
-# app.register_blueprint(colaborador_EPIs_bp, url_prefix='/colaborador_epi')
+app.register_blueprint(Epis_bp, url_prefix='/epi')
 # app.register_blueprint(colaboradores_EPIs_bp, url_prefix='/epis')
 
 if __name__ == "__main__":
---
+++ API/rotas/epi.py
@@ -0,0 +1,157 @@
+import decimal
+from flask import Blueprint, jsonify, request
+from conexao import criar_conexao, fechar_conexao
+
+Epis_bp = Blueprint('/epi', __name__)
+
+dados_necessarios = ["nome_epi", "validade", "descricao", "categoria", "foto"]
+
+def CheckValueNotIn(keys = [], obj = {}):
+    for i in keys:
+        if i not in obj:
+            return True
+    return False
+
+@Epis_bp.route('/listar', methods=['GET'])
+def ListarEPIs():
+    conexao = criar_conexao()
+    cursor = conexao.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM EPIs")
+    EPIs = cursor.fetchall()
+
+    cursor.close()
+    conexao.close()
+
+    return jsonify(EPIs)
+
+@Epis_bp.route('/buscar/<string:nome>', methods=['GET'])
+def buscarEPIsNome(nome):
+    conexao = criar_conexao()
+    cursor = conexao.cursor(dictionary=True)
+
+    consulta = "SELECT * FROM EPIs WHERE nome_epi like %s"
+
+    cursor.execute(consulta, (f'%{nome}%', ))
+
+    EPIs = cursor.fetchall()
+    
+    cursor.close()
+    conexao.close()
+
+    return jsonify(EPIs)
+
+@Epis_bp.route('/buscar/<int:id>', methods=['GET'])
+def buscarEPIsId(id):
+    conexao = criar_conexao()
+    cursor = conexao.cursor(dictionary=True)
+
+    consulta = "SELECT * FROM EPIs WHERE id = %s"
+
+    cursor.execute(consulta, (id, ))
+
+    Epi = cursor.fetchone()
+    
+    cursor.close()
+    conexao.close()
+
+    return jsonify(Epi)
+
+@Epis_bp.route('/cadastrar', methods=['POST'])
+def cadastrarEPIs():
+    novoEpi = request.get_json()
+    
+    if CheckValueNotIn(dados_necessarios,novoEpi):
+        return jsonify({'status': 'error', "message": 'dados imcompletos'}), 400
+    
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+
+    try:
+        campos = []
+        keys = []
+        valores = []
+        
+        for dado in novoEpi:
+            campos.append(dado)
+            valores.append(f'%s')
+            keys.append(novoEpi[dado])
+            
+        comando = f'INSERT INTO EPIs ({",".join(campos)}) VALUES ({",".join(valores)})'
+        cursor.execute(comando, keys)
+        
+        conexao.commit()
+        status = {'status':'success', 'code': 201}
+        
+    except Exception as e:
+        conexao.rollback()
+        status = {'status': 'error', 'message': str(e)}
+        
+    finally:
+        cursor.close()
+        fechar_conexao(conexao)
+    
+    return jsonify(status)
+
+@Epis_bp.route('/alterar/<int:id>', methods=['PUT'])
+def alterarEPI(id):
+    conexao = criar_conexao()
+    cursor = conexao.cursor(dictionary=True)
+
+    dados = request.get_json()
+    try:
+        campos_para_atualizar = []
+        valores_para_atualizar = []
+        
+        
+        for dado in dados_necessarios:
+            campos_para_atualizar.append(f'{dado} = %s')
+            valores_para_atualizar.append(dados[dado])
+
+      
+        if not campos_para_atualizar:
+            return jsonify({'status': 'error', 'message': 'Nenhum campo fornecido para atuaização'}), 400
+        
+        comando = "UPDATE EPIs SET "+",".join(campos_para_atualizar) + "WHERE id = %s"
+        valores = valores_para_atualizar + [id]
+        cursor.execute(comando,valores)
+        conexao.commit()
+
+        status = {'status': 'success', 'message': 'colaborador_EPIs atualizado com sucesso'}
+        return jsonify(status)
+    except Exception as e:
+        conexao.rollback()
+        status = {'status': 'error', 'message': str(e)} 
+
+    finally:
+        cursor.close()
+        fechar_conexao(conexao)
+
+@Epis_bp.route('/deletar/<int:id>', methods=['DELETE'])
+def deletarEPI(id):
+    conexao = criar_conexao()
+    cursor =  conexao.cursor()
+
+    epi = cursor.execute("SELECT COUNT(*) FROM EPIs WHERE id = %s", (id,))
+
+    if epi == 0:
+        return jsonify({'status': 'error', 'message': 'id não encontrado'}), 404
+    
+    try:
+        comando = 'DELETE FROM EPIs WHERE id = %s'
+        
+        cursor.execute(comando, (id,))
+        
+        conexao.commit()
+        
+        status = {'status': 'success', 'message': 'Epi deletado com sucesso'}
+        
+    except Exception as e:
+        conexao.rollback()
+        status = {'status': 'error', 'message': str(e)}
+        
+    finally:
+        cursor.close()
+        conexao.close()
+    
+    return jsonify(status)
